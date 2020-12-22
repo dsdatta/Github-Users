d1481d75ca33b29dc0b9ed11292477d962c676a3
dsdatta <sameer.sathy@gmail.com>
2020-12-22 12:53:04 +0100

    Commenting and Code clean-up

--- src/app/github/github.component.ts
+++ src/app/github/github.component.ts
@@ -18,10 +18,7 @@ export class GithubComponent  {
   constructor(private _githubService:ServerComponent) {
       this._githubService.getUser().subscribe(user => {
      this.user = false;
-     //this.user = user;
-     //this.avatar_url = this.user.avatar_url;
-     //this.animationFunction(this.user.avatar_url);
- 
+     //this.user = user; 
      },errors => {
         this.errors = errors;
        console.error('Oops', errors);
@@ -42,15 +39,13 @@ export class GithubComponent  {
   {   
   var scene = new THREE.Scene();
 
-  //var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000)
   var camera = new THREE.PerspectiveCamera(75,400/300,0.1,1000)
 
         camera.position.z =1.5;
 
   var renderer = new THREE.WebGLRenderer({antialias: true});
-  //renderer.setClearColor("#e5e5e5");
   renderer.setClearColor("#FFFFFF");
-  //renderer.setSize(window.innerWidth,window.innerHeight);
+
   renderer.setSize(400,300);
   var elementExists = document.getElementById("one");
 
@@ -67,8 +62,6 @@ export class GithubComponent  {
     
 
   window.addEventListener('resize', () => {
-            //renderer.setSize(window.innerWidth,window.innerHeight);
-            //camera.aspect = window.innerWidth / window.innerHeight;
             renderer.setSize(400,300);
             camera.aspect = 400 / 300;
 
@@ -76,38 +69,31 @@ export class GithubComponent  {
         })
 
   var geometry = new THREE.BoxGeometry(1, 1, 1);
-  //var material = new THREE.MeshLambertMaterial({color: 0xFFCC00});
-  //var material = new THREE.MeshLambertMaterial({color: 0xF7F7F7});
+
+  //Create texture loader to load avatar image of the searched user.
   const textureLoader = new THREE.TextureLoader();
   const texture = textureLoader.load(String(avatar_url));
   var materials = new THREE.MeshLambertMaterial({map: texture });
 
   var mesh = new THREE.Mesh(geometry, materials);
- // mesh.position.x=-1;
- // mesh.position.y=1;
-  //mesh.position.z=1;
-  // mesh.rotation.set(45,0,0);
-   //mesh.scale.set(1,2,1);
+
   scene.add(mesh);
 
   var light = new THREE.PointLight(0xFFFFFF, 1, 500)
         light.position.set(10,0,25);
         scene.add(light);
+
   var render =function(){
     requestAnimationFrame(render);
-    //mesh.rotation.x +=-0.01;
-    // mesh.rotation.y +=0.01;
      mesh.rotation.y +=0.02;
     renderer.render(scene,camera);
   }
   render();
-  
 
   }
   
   ngOnInit(): void {
-  
-     
+ 
   }
 
 }
